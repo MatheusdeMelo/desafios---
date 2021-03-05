029803a045120b3265f7ad1a6c5e0c61872402b5
MatheusdeMelo <matheusdemelopf@gmail.com>
2021-03-05 19:06:02 -0300

    desafio da aula 19 Final
    versão final

--- deafio dessa semana/aula 93.py
+++ deafio dessa semana/aula 93.py
@@ -1,10 +1,15 @@
-dados_centrais = {'nome': input('Qual é seu nome?: '),'partidas': int(input('Quantas partidas vc jogou?: '))}
-print(type(dados_centrais['partidas']))
-golssssssssssssssss = []
-total_de_gols = []
-for c in range(dados_centrais['partidas']):
-    golssssssssssssssss.append(int(input(f'na sua {c + 1}° vc fez quantos gols?: ')))
-    dados_centrais['gols'] = golssssssssssssssss
-for gols in dados_centrais['gols']:
-    total_de_gols += gols
-print(total_de_gols)
+temporada = {'nome':  input('digite o nome do jogador: ')}
+partidas = int(input('digite o número de partidas: '))
+numero_gols = []
+contador_bala = 0
+for c in range(partidas):
+    numero_gols.append(int(input(f'qual foi o número de gols feito na °{c + 1} partida: ')))
+for gols in numero_gols:
+    contador_bala += gols
+temporada['gols'] = contador_bala
+temporada['numero de partidas'] = partidas
+temporada['porcentagem'] = round(contador_bala / partidas, 2)
+print(f'''Dados da temporada do {temporada['nome']}:
+Ele jogou {temporada['numero de partidas']} partidas e nesse numero de partidas ele fez {temporada['gols']}
+a média dele foi de {temporada['porcentagem']} gols por partida
+''')
