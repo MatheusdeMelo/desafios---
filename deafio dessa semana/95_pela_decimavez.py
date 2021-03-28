9800543fc2405163213e338818446c82f6e4edcb
MatheusdeMelo <matheusdemelopf@gmail.com>
2021-03-28 13:34:41 -0300

    955555555555555555555555
    funcionou tiooooooooooooooooooooooooooooo

---
+++ deafio dessa semana/95_pela_decimavez.py
@@ -0,0 +1,18 @@
+temporada = {}
+lista = []
+gols = 0
+contador = 0
+while True:
+    temporada = {'nome': input('digite o nome: '), 'partidas': int(input('quantas partidas ele jogou?: ')), 'gols': []}
+    for g in range(temporada['partidas']):
+        gols += int(input(f'quantos gols ele fez na °{g + 1} partida: '))
+    temporada['gols'].append(gols)
+    lista.append(temporada)
+    pergunta = input('quer continuar S/N?: ')
+    contador += 1
+    if pergunta == 'N':
+        break
+temporada['média'] = round(contador / temporada['partidas'], 2)
+print(f"""Dados da Temporada:
+{lista}
+""")
