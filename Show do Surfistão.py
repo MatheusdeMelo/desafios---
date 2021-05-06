a66d3547d28d93b8cd9bfc8e27e4dec82beefe0f
MatheusdeMelo <matheusdemelopf@gmail.com>
2021-05-06 12:01:37 -0300

    terceiro Show do Surfistão
    Tio Jusitn me ajuda pq n to conseguindo dar run ;-;

--- Show do Surfistão.py
+++ Show do Surfistão.py
@@ -1,16 +1,40 @@
 def coloque(dic, name, coisa):
     dic[name] = coisa
+
+
 def listar(um, dois, tres, quatro, cinco):
+    global resultado
     lista = []
     lista.append(um)
     lista.append(dois)
     lista.append(tres)
     lista.append(quatro)
     lista.append(cinco)
-    str(input(random.choice(lista)))
+    resultado = str(input(random.choice(lista)))
+
+
+
+def conferir(a):
+   respostas = []
+   respostas.append(dict1['resp certa'])
+   respostas.append(dict2['resp certa'])
+   respostas.append(dict3['resp certa'])
+   respostas.append(dict4['resp certa'])
+   respostas.append(dict5['resp certa'])
+   if resultado != respostas[1]:
+    if int(resultado) in respostas:
+        print('você acertou!!')
+    else:
+        print('você errou!!')
+   if resultado == respostas[1]:
+       if resultado in respostas:
+           print('você acertou!!')
+       else:
+           print('você errou!!')
 
 
 import random
+resultado = ''
 dict1 = {'pergunta': 'raiz quadrada de 36: ', 'resp certa': 6, 'resp errada1': 18, 'resp errada2': 3}
 dict2 = {'pergunta': 'qual é a regra utilizada pra resolver equações de 2°: ', 'resp certa': 'formula de Bhaskara','resp errada1': 'formula de Newton', 'resp errada2': 'teorema de Pitágoras'}
 dict3 = {'pergunta': 'reposta de 22 + 3 x (2 x 5) - 4: ', 'resp certa': 48, 'resp errada1': 246, 'resp errada2': 72}
@@ -23,5 +47,7 @@ coloque(matematica, '3°', dict3)
 coloque(matematica, '4°', dict4)
 coloque(matematica, '5°', dict5)
 #lista = [matematica, geografia, historia, curiosidades, ciências]
-listar(matematica['1°']['pergunta'], matematica['2°']['pergunta'], matematica['3°']['pergunta'], matematica['4°']['pergunta'], matematica['5°']['pergunta'])
-print()
+for c in range(5):
+    listar(matematica['1°']['pergunta'], matematica['2°']['pergunta'], matematica['3°']['pergunta'], matematica['4°']['pergunta'], matematica['5°']['pergunta'])
+    conferir(resultado)
+    print(c)
