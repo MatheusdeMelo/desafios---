b4aed34cad740b61cd9dcab5b3c5401a409fe072
MatheusdeMelo <matheusdemelopf@gmail.com>
2021-06-02 06:58:34 -0300

    Pseudocódigo
    o Pseudocódigo, Tio

---
+++ deafio dessa semana/Pseudo.py
@@ -0,0 +1,24 @@
+#Funções:
+#   Colocar em um dicionário
+#       Colocar perguntas
+#       Colocar respostas certas e erradas
+#   Recebida a resposta
+#       Conferir se consta nas respostas certas
+#       Retirar a pergunta da lista de escolhas
+#
+#Antes das funções:
+#   Criar os dicionários
+#       Criar perguntas
+#       Criar repostas certas e erradas
+#
+#Depois das funções
+#   Escolher uma das perguntas com Random
+#   Mostrar para o jogador a pergunta
+#   Receber a resposta
+#   Depois da resposta mostrar outra pergunta
+#       Loop
+#   Fim do loop
+#   Trocar de Matéria
+#       Novo loop
+#   Fim de jogo
+#       Mostrar quantas respostas ele acertou e quanto ele ganhou 
